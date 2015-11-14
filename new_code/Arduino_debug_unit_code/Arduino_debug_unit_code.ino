d717b381336469c9e05247651fb0a818e23fed79
robbotorigami <robbotorigami@gmail.com>
2015-11-14 13:37:30 -0700

    Added Arduino debug unit code

--- new_code/Arduino_debug_unit_code/Arduino_debug_unit_code.ino
+++ new_code/Arduino_debug_unit_code/Arduino_debug_unit_code.ino
@@ -1,31 +1,35 @@
 /*
   This code writes from the serial in to an LCD.
-  Created for A-Team S-Band system for COSGC
+  Designed for debug unit of RF Beacon system for COSGC
   Robert Belter 10/30/2015
  */
 
-// include the library code:
+String toDisplay = "RF Beacon Debug Unit";
+//If using a 16x2 LCD, uncommment the below line
+//toDisplay = "Debug Unit";
+
 #include <LiquidCrystal.h>
 
 // initialize the library with the numbers of the interface pins
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 void setup() {
-  // set up the LCD's number of columns and rows:
+  // Set up columuns and rows. Note: We used a 20x4 LCD, 16x2 is more common
   lcd.begin(20, 4);
+  
   // Print a message to the LCD.
-  lcd.print("RF Beacon Debug Unit");
+  lcd.print(toDisplay);
+
   delay(1000);
   Serial.begin(57600);
 }
 
 void loop() {
   // set the cursor to column 0, line 1
-  // (note: line 1 is the second row, since counting begins with 0):
   lcd.setCursor(0, 1);
   if(Serial.available()){
     lcd.clear();
-    lcd.print("RF Beacon Debug Unit");
+    lcd.print(toDisplay);
   }
   while(Serial.available()){
     lcd.write(Serial.read());
